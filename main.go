6ba5d8ba9204017572e25855d21e1f411ff475ae
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2020-03-22 15:04:42 +0100

    extraction to packages/files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"deal-hunter/io"
 	"deal-hunter/scrapers"
+	"flag"
 	"fmt"
 	"github.com/google/logger"
 	"github.com/spf13/viper"
@@ -14,7 +15,11 @@ import (
 
 var bot io.TelegramBot
 
+var configFolder = flag.String("configFolder", ".", "full path to config folder")
+var logFolder = flag.String("logFolder", "logs", "full path to log folder")
+
 func main() {
+	flag.Parse()
 	defer setUpLogger().Close()
 
 	logger.Info("Application start")
@@ -43,14 +48,13 @@ func main() {
 func getConfig() {
 	logger.Info("Config parsing start")
 
-	viper.SetConfigName("config")    // name of config file (without extension)
-	viper.AddConfigPath(".")         // optionally look for config in the working directory
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(*configFolder)
 	viper.AddConfigPath("resources") // optionally look for config in the working directory
 	err := viper.ReadInConfig()      // Find and read the config file
 	if err != nil {                  // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
-
 	logger.Info("Config parsing end")
 }
 
@@ -80,9 +84,9 @@ func handleDeals(deals []*scrapers.Deal) {
 }
 
 func setUpLogger() *logger.Logger {
-	_ = os.Mkdir("logs", os.ModeDir)
-	_ = os.Chmod("logs", os.ModePerm)
-	filename := "logs/" + time.Now().Format("2006_01_02-15_04") + ".log"
+	_ = os.Mkdir(*logFolder, os.ModeDir)
+	_ = os.Chmod(*logFolder, os.ModePerm)
+	filename := *logFolder + "/" + time.Now().Format("2006_01_02-15_04") + ".log"
 	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		logger.Error(err)
